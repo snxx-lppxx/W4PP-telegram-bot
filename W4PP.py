ed14f11a32c2e8725bd2eae77a8c444f610e0f67
snxx-lppxx <snmgreenx@gmail.com>
2021-03-22 20:31:54 +0500

    Initial commit

---
+++ W4PP.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import requests
+import re
+import asyncio
+
+from source.config.config import token
+
+class Main:
+	def __init__():
+		pass
+
+	def main():
+		pass
+
+if __name__ == '__main__':
+	main()
+	
